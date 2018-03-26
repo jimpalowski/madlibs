8ec38864e66394fbfe311520f15380a8b47d523d
Eric Wolfe and Jim Palowski <jim@>
2018-03-26 13:41:52 -0700

    refactored jquery in madlibs

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var nameInput = $("input#name").val();
+    //var nameInput = $("input#name").val();
+    var list = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".name").text(nameInput);
-
-
+    list.forEach(function(lists) {
+      var userInput = $("input#" + lists).val();
+      $("." + lists).text(userInput);
+    });
     $("#letter").show();
-
     event.preventDefault();
   });
 });
